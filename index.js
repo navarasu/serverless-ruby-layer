42f33a2f343d6b0642f00dcda7d0cbd08f79bb1d
Rob Kaufman <rob@notch8.com>
2023-05-08 09:48:41 -0700

    allow setting docker image name, skip build if gemfile or gemfile.lock have not changed

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 const { bundleGems, excludePackage } = require('./lib/bundle');
+const path = require('path');
 
 class ServerlessRubyBundler {
   get options() {
     const options = Object.assign(
       {
         use_docker: false,
-        ignore_gemfile_lock: false
+        ignore_gemfile_lock: false,
+        docker_image_name: 'ruby-layer:docker'
       },
       (this.serverless.service.custom &&
         this.serverless.service.custom.rubyLayer) ||
@@ -18,6 +20,9 @@ class ServerlessRubyBundler {
     this.serverless = serverless;
     this.servicePath = this.serverless.config.servicePath;
     this.warningLogged = false;
+    this.changed = true;
+    this.ruby_layer = path.join(this.servicePath,'.serverless','ruby_layer')
+    this.cli = this.serverless.cli
 
     this.commands = {
       rubylayer: {
